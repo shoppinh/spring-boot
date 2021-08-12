28b93b269d98e7dd144620677cc38038646c9851
shoppinh <mactrungkien2000@gmail.com>
2021-08-12 23:04:51 +0700

    init spring boot project

---
+++ src/main/java/com/kienneik/api/NewsAPI.java
@@ -0,0 +1,29 @@
+package com.kienneik.api;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kienneik.DTO.NewsDTO;
+
+@RestController
+public class NewsAPI {
+	@PostMapping(value = "/news")
+	public NewsDTO createNews(@RequestBody NewsDTO model) {
+		return model;
+	}
+	
+	@PutMapping(value = "/news")
+	public NewsDTO updateNews(@RequestBody NewsDTO model) {
+		return model;
+	}
+	
+	@DeleteMapping(value = "/news")
+	public void deleteNews(@RequestBody long[] ids) {
+	}
+	
+	
+
+}
